d705470794cc7ff728f1779efa8b8a7b9472b1ce
rm <recussivef@gmail.com>
2019-11-14 17:58:01 +0300

    feat: numbers in kotlin

---
+++ src/basics.kt
@@ -0,0 +1,18 @@
+fun main(args: Array<String>){
+    val a: Int = 10000
+    val d: Double = 100.00
+    val f: Float = 100.00f
+    val l: Long = 1000000004
+    val s: Short = 10
+    val b: Byte = 1
+
+
+
+
+    println("Your Int Value is: " +a)
+    println("YOur Double value is: "+d)
+    println("Your float value is: " +f)
+    println("Your Long value is: " +l)
+    println("Your short value is" + s)
+    println("Your byte value is: "+b)
+}
